60f1d95d753df0579122cbea87047609ccf93870
Yuan-Man <68322456+Yuan-ManX@users.noreply.github.com>
2025-05-07 16:14:20 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,1 +1,23 @@
+from .nodes import LoadAudioXModel, LoadAudioXVideo, Prompt, LoadAudioXAudio, Condition, Generate, SaveAudioXAudio
 
+NODE_CLASS_MAPPINGS = {
+    "LoadAudioXModel": LoadAudioXModel,
+    "LoadAudioXVideo": LoadAudioXVideo,
+    "Prompt": Prompt,
+    "LoadAudioXAudio": LoadAudioXAudio,
+    "Condition": Condition,
+    "Generate": Generate,
+    "SaveAudioXAudio": SaveAudioXAudio,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "LoadAudioXModel": "Load AudioX Model",
+    "LoadAudioXVideo": "Load AudioX Video",
+    "Prompt": "Prompt",
+    "LoadAudioXAudio": "Load AudioX Audio",
+    "Condition": "Condition",
+    "Generate": "Generate",
+    "SaveAudioXAudio": "Save AudioX Audio",
+} 
+
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
